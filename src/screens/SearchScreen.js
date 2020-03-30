fe3ea18c61ccaf07baa46ca0c2d9c67447b41cd9
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-03-30 16:24:27 +0800

    Working on seacrchapi

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../../src/components/SearchBar';
+import yelp from '../../src/api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
+  const [result, setResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses)
+    } catch(err) {
+      setErrorMessage('Something went wrong')
+    }
+  }
+
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
 
   return (
     <View style={styles.background}>
       <SearchBar
         term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log("onTermSubmit")}
+        onTermChange={setTerm}
+        onTermSubmit={searchApi}
       />
-    <Text>Search Screen</Text>
-    <Text>{term}</Text>
+    {errorMessage ? <Text>{errorMessage}</Text> : null}
+    <Text>We have found {result.length} results</Text>
     </View>
   );
 };
