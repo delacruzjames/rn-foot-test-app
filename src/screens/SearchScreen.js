0a9cc72ea8a6c50ff1e1a432b9c552c322174602
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-03-30 20:37:21 +0800

    Update empty elements

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 // Components
 import SearchBar from '../../src/components/SearchBar';
 import ResultsList from '../../src/components/ResultsList';
@@ -16,32 +16,34 @@ const SearchScreen = () => {
     })
   }
   return (
-    <View style={styles.background}>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={searchApi}
       />
-    {errorMessage ? <Text>{errorMessage}</Text> : null}
-    <Text>We have found {results.length} results</Text>
-    <ResultsList
-      title="Cost effective"
-      results={filterByPrice('$')}
-    />
-    <ResultsList
-      title="Bt pricer"
-      results={filterByPrice('$$')}
-    />
-    <ResultsList
-      title="Big spender"
-      results={filterByPrice('$$$')}
-    />
-    </View>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <ScrollView>
+        <ResultsList
+          title="Cost effective"
+          results={filterByPrice('$')}
+        />
+        <ResultsList
+          title="Bt pricer"
+          results={filterByPrice('$$')}
+        />
+        <ResultsList
+          title="Big spender"
+          results={filterByPrice('$$$')}
+        />
+      </ScrollView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
+    flex: 1
     // backgroundColor: '#ffffff'
   }
 });
