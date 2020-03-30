9bcbaf4975700c0c34096733405e9a3f41d3883b
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-03-30 17:26:56 +0800

    Added results list

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+// Components
 import SearchBar from '../../src/components/SearchBar';
-import yelp from '../../src/api/yelp';
+import ResultsList from '../../src/components/ResultsList';
+// Hooks
 import useResults from '../../src/hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [searchApi, results, errorMessage] = useResults();
 
+  const filterByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price;
+    })
+  }
   return (
     <View style={styles.background}>
       <SearchBar
@@ -17,6 +24,18 @@ const SearchScreen = () => {
       />
     {errorMessage ? <Text>{errorMessage}</Text> : null}
     <Text>We have found {results.length} results</Text>
+    <ResultsList
+      title="Cost effective"
+      results={filterByPrice('$')}
+    />
+    <ResultsList
+      title="Bt pricer"
+      results={filterByPrice('$$')}
+    />
+    <ResultsList
+      title="Big spender"
+      results={filterByPrice('$$$')}
+    />
     </View>
   );
 };
